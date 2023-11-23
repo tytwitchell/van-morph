958c443dba32928a886fe1f3fc841066e88de7ee
tytwitchell <tytwitchell@gmail.com>
2023-11-23 12:13:05 -0500

    chore: vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  build: {
+  rollupOptions: {
+    external: ['react-icons/Ai'],
+  },
+},
   
 })
 
