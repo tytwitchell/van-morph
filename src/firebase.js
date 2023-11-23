0789ad45bbcb6d9c165d2022c429a60eddab7dad
tytwitchell <tytwitchell@gmail.com>
2023-11-23 10:52:22 -0500

    refactor: gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -9,23 +9,27 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
-import { initializeApp } from "firebase/app";
+import { app, db, q, querySnapshot } from "./initfs";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDQZYBA58U7Xm5V2q0yMpuqvCKcXAtVbVo",
-  authDomain: "van-morph-engine.firebaseapp.com",
-  projectId: "van-morph-engine",
-  storageBucket: "van-morph-engine.appspot.com",
-  messagingSenderId: "669932279923",
-  appId: "1:669932279923:web:9c00e7393cecd5e8b4b206",
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// import { initializeApp } from "firebase/app";
 
-const q = query(collection(db, "vans"), where("maxSeats", "==", 6));
-const querySnapshot = await getDocs(q);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDQZYBA58U7Xm5V2q0yMpuqvCKcXAtVbVo",
+//   authDomain: "van-morph-engine.firebaseapp.com",
+//   projectId: "van-morph-engine",
+//   storageBucket: "van-morph-engine.appspot.com",
+//   messagingSenderId: "669932279923",
+//   appId: "1:669932279923:web:9c00e7393cecd5e8b4b206",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// const q = query(collection(db, "vans"), where("maxSeats", "==", 6));
+// const querySnapshot = await getDocs(q);
+
 const vansInDb = [];
 
 async function addToDb(formData) {
